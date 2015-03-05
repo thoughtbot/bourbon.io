7677db3e8aecfcad26b2befc08ad07d012e4af42
Tyson Gach <tyson@thoughtbot.com>
2015-03-05 15:09:45 -0500

    Dynamically generate docs version pages
    - Uses Middleman's dynamic pages feature: https://middlemanapp.com/advanced/dynamic_pages

--- config.rb
+++ config.rb
@@ -2,9 +2,13 @@ set :css_dir, "assets/stylesheets"
 set :fonts_dir, "assets/fonts"
 set :images_dir, "assets/images"
 set :js_dir, "assets/javascripts"
-set :markdown_engine, :redcarpet
-set :markdown, :smartypants => true
+# set :markdown_engine, :redcarpet
+# set :markdown, :smartypants => true
 set :relative_links, true
+
+["4_2_1", "5_0_0"].each do |version|
+  proxy "/docs/#{version}.html", "/docs/index.html", :ignore => true
+end
 
 activate :autoprefixer do |config|
   config.browsers = ["> 1%", "last 5 versions", "Explorer >= 9"]
