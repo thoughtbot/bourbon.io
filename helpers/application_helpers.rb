001e68a8fb779e85a25dccf39b3bf5ad0dcc9277
Tyson Gach <tyson@thoughtbot.com>
2016-04-06 19:37:11 -0400

    Add SVG helper

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -1,21 +1,14 @@
 module ApplicationHelpers
-  # def icon(name)
-  #   root = Middleman::Application.root
-  #   file_path = "#{root}/source/images/icons/#{name}.svg"
-  #   return File.read(file_path) if File.exists?(file_path)
-  #   "(not found)"
-  # end
-
   def markdown(contents)
     renderer = Redcarpet::Render::HTML
     markdown = Redcarpet::Markdown.new(renderer, extensions = {})
     markdown.render(contents)
   end
 
-  # def svg(name)
-  #   root = Middleman::Application.root
-  #   file_path = "#{root}/source/images/#{name}.svg"
-  #   return File.read(file_path) if File.exists?(file_path)
-  #   "(not found)"
-  # end
+  def svg(name)
+    root = Middleman::Application.root
+    file_path = "#{root}/source/images/#{name}.svg"
+    return File.read(file_path) if File.exists?(file_path)
+    "(not found)"
+  end
 end
