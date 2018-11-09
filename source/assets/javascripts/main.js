26c5a2e3d82715284f932a1704ef915b2ace9fea
Tyson Gach <tyson@thoughtbot.com>
2018-11-09 16:22:30 -0500

    Check if dropdown exists

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,8 +1,10 @@
 const dropdownButton = document.querySelector('[data-js="dropdown"]');
 
-dropdownButton.addEventListener('click', function() {
-  let expanded = this.getAttribute('aria-expanded') === 'true' || false;
-  this.setAttribute('aria-expanded', !expanded);
-  let menu = this.nextElementSibling;
-  menu.hidden = !menu.hidden;
-});
+if (dropdownButton) {
+  dropdownButton.addEventListener('click', function() {
+    let expanded = this.getAttribute('aria-expanded') === 'true' || false;
+    this.setAttribute('aria-expanded', !expanded);
+    let menu = this.nextElementSibling;
+    menu.hidden = !menu.hidden;
+  });
+}
