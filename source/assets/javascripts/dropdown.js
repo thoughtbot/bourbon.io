9a6cb0924745fd365c8f1701aea49eb6c4b930c3
Tyson Gach <tyson@thoughtbot.com>
2018-11-09 14:39:01 -0500

    Simplify JavaScript include

--- source/assets/javascripts/dropdown.js
+++
@@ -1,8 +0,0 @@
-const dropdownButton = document.querySelector('[data-js="dropdown"]');
-
-dropdownButton.addEventListener('click', function() {
-  let expanded = this.getAttribute('aria-expanded') === 'true' || false;
-  this.setAttribute('aria-expanded', !expanded);
-  let menu = this.nextElementSibling;
-  menu.hidden = !menu.hidden;
-});
--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,1 +1,8 @@
-//= require dropdown
+const dropdownButton = document.querySelector('[data-js="dropdown"]');
+
+dropdownButton.addEventListener('click', function() {
+  let expanded = this.getAttribute('aria-expanded') === 'true' || false;
+  this.setAttribute('aria-expanded', !expanded);
+  let menu = this.nextElementSibling;
+  menu.hidden = !menu.hidden;
+});
